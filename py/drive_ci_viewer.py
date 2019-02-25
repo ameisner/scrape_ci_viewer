b22081fee2a342a46f482445d676bdc4e9861025
Aaron Meisner <ameisner@fas.harvard.edu>
2019-02-25 13:40:50 -0700

    generalize to handle input ra, dec coordinates

--- py/drive_ci_viewer.py
+++ py/drive_ci_viewer.py
@@ -2,15 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-def get_ci_image_urls(verbose=False):
+def get_ci_image_urls(ra, dec, verbose=False):
+
+    # ra, dec should be in degrees and each should be
+    # just a single scalar value, not a list/array
+
+    assert((ra >= 0) and (ra < 360))
+    assert((dec >= -90) and (dec <= 90))
+
     driver = webdriver.PhantomJS()
     driver.get("http://legacysurvey.org/viewer-dev/ci")
     elem_ra = driver.find_element_by_id("ra_input")
     elem_ra.clear()
-    elem_ra.send_keys("200.1")
+    elem_ra.send_keys(str(ra))
     elem_dec = driver.find_element_by_id("dec_input")
     elem_dec.clear()
-    elem_dec.send_keys("-10.5")
+    elem_dec.send_keys(str(dec))
     elem_go = driver.find_element_by_id("radec_submit")
     elem_go.click()
 
