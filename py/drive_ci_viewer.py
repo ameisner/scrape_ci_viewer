4250de70700aa3cd11a00ca5b176935bb83b7194
Aaron Meisner <ameisner@fas.harvard.edu>
2019-02-25 13:18:40 -0700

    restructure

--- py/drive_ci_viewer.py
+++ py/drive_ci_viewer.py
@@ -1,30 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.PhantomJS()
 
-driver.get("http://legacysurvey.org/viewer-dev/ci")
+def get_ci_image_urls(verbose=False):
+    driver = webdriver.PhantomJS()
+    driver.get("http://legacysurvey.org/viewer-dev/ci")
+    elem_ra = driver.find_element_by_id("ra_input")
+    elem_ra.clear()
+    elem_ra.send_keys("200.1")
+    elem_dec = driver.find_element_by_id("dec_input")
+    elem_dec.clear()
+    elem_dec.send_keys("-10.5")
+    elem_go = driver.find_element_by_id("radec_submit")
+    elem_go.click()
 
-elem_ra = driver.find_element_by_id("ra_input")
+    if verbose:
+        print(driver.page_source)
 
-elem_ra.clear()
+    elem_image_w = driver.find_element_by_id("image_CIW")
 
-elem_ra.send_keys("200.1")
+    url_w = elem_image_w.get_attribute("src")
 
-elem_dec = driver.find_element_by_id("dec_input")
+    driver.close()
 
-elem_dec.clear()
-
-elem_dec.send_keys("-10.5")
-
-elem_go = driver.find_element_by_id("radec_submit")
-
-elem_go.click()
-
-print(driver.page_source)
-
-elem_image_w = driver.find_element_by_id("image_CIW")
-
-url_w = elem_image_w.get_attribute("src")
-
-driver.close()
+    return url_w
